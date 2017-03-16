a1146f82c8427c6384d3dd2f75c4adcbaae61672
Rameshwar Bhaskaran <rameshwar.zorro@gmail.com>
2017-03-16 23:18:36 +0530

    add

---
+++ cab_sharing/static/main.js
@@ -0,0 +1,10 @@
+// Refresh the Table every 20 seconds
+(function update(){
+    $.ajax({    
+       url: "/cabsharing/",
+          success: function(data) {
+          $('#the_table').html(data);
+          }
+          setTimeout(update,10000)
+    });
+})();
