c54dc3d9ea3da1719267d96df8775383f6c9345d
Rameshwar Bhaskaran <rameshwar.zorro@gmail.com>
2017-03-24 23:36:42 +0530

    Added postgres

--- cab_sharing/settings.py
+++ cab_sharing/settings.py
@@ -77,10 +77,9 @@ WSGI_APPLICATION = 'cab_sharing.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 
